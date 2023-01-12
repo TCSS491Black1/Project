0701007b0f65b760a6a0ab1127069e18bbd542b6
Patrick Tibbals <tibbalsp@gmail.com>
2023-01-11 18:16:46 -0800

    CharacterController

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
-const gameEngine = new GameEngine();
+var gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
 
+ASSET_MANAGER.queueDownload("")
+ASSET_MANAGER.queueDownload("")
+
 ASSET_MANAGER.downloadAll(() => {
-	const canvas = document.getElementById("gameWorld");
-	const ctx = canvas.getContext("2d");
+	var canvas = document.getElementById("gameWorld");
+	var ctx = canvas.getContext("2d");
+	
+	params.canvasWidth = 1024
+	params.canvasHeight = 768;
+
 
 	gameEngine.init(ctx);
+
+	gameEngine.addEntity(new SceneManager(gameEngine));
 
 	gameEngine.start();
 });
